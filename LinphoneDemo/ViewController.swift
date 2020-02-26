25ebbb512dc1cbcbd5d17b4a99d74d212c222eed
vladimirasantsev <tawnysoul@gmail.com>
2020-02-26 22:45:42 +0700

    Initial

---
+++ LinphoneDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LinphoneDemo
+//
+//  Created by Vladimir Asantsev on 26.02.2020.
+//  Copyright © 2020 Vladimir Asantsev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
